0e9791797027c1e3917c8cb0c45b3752b69787fc
serginjo-m <serginjoredline@gmail.com>
2024-02-24 20:12:24 +0100

    Puzzle working solution (weak)
    This is a basic puzzle solution, without any architectural improvements. Just a weak, but working code.

--- PuzzleGame/PuzzleViewController.swift
+++ PuzzleGame/PuzzleViewController.swift
@@ -23,9 +23,6 @@ class PuzzleViewController: UIViewController{
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         
         collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
-        
-        
-        
         collectionView?.register(PuzzleCell.self, forCellWithReuseIdentifier: "cell")
         //-->
         collectionView?.dragInteractionEnabled = true
@@ -49,79 +46,63 @@ class PuzzleViewController: UIViewController{
 //        collectionView?.frame = view.bounds
         collectionView?.translatesAutoresizingMaskIntoConstraints = false
         collectionView?.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        collectionView?.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        collectionView?.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        collectionView?.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        collectionView?.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         collectionView?.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        
-        print("is landscape? ->", UIDevice.current.orientation.isLandscape)
-//        
-//        let newWidth = view.bounds.width
-//        let newHeight = view.bounds.height
-//        if newWidth > newHeight{
-//            print("horizontal")
-//        }else{
-//            print("vertical")
-//        }
-        collectionView?.collectionViewLayout.invalidateLayout()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        guard let layout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout else {return}
+        layout.invalidateLayout()
+        collectionView?.reloadData()
     }
 
 }
 
 extension PuzzleViewController: UICollectionViewDataSource {
-    //+
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //return colors.count
+        
         return puzzle.unSolvedImages.count
     }
-    //+
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PuzzleCell
         
-//        cell.imageView.image = UIImage(named: puzzle.unSolvedImages[indexPath.item])
-//        let sortedItem = puzzle.solvedImages[indexPath.item]
-//        let unsortedItem = puzzle.unSolvedImages[indexPath.item]
-        //        if sortedItem == unsortedItem {
-//            print("this is right place: " + sortedItem + " " + unsortedItem)
-//        }
-        
-        
-        
-        
-        //TODO: height - when landscape, width - when portrait
-        var third: CGFloat = 0
-        var twoThirds: CGFloat = 0
-        if UIDevice.current.orientation.isLandscape {
-            third = -view.bounds.height * 0.33
-            twoThirds = -view.bounds.height * 0.66
-        }else{
-            third = -view.bounds.width * 0.33
-            twoThirds = -view.bounds.width * 0.66
-        }
+        if let puzzleWidth = self.collectionView?.frame.width, let puzzleHeight = self.collectionView?.frame.height {
+            let thirdWidth = -puzzleWidth * 0.33
+            let twoThirdsWidth = -puzzleWidth * 0.66
+            let thirdHeight = -puzzleHeight * 0.33
+            let twoThirdsHeight = -puzzleHeight * 0.66
 
-        
-        switch puzzle.unSolvedImages[indexPath.item] {
-        
-        case "1":
-            cell.template = (0, 0)
-        case "2":
-            cell.template = (0, third)//TODO: That's not the perfect pixel solution
-        case "3":
-            cell.template = (0, twoThirds)
-        case "4":
-            cell.template = (third, 0)
-        case "5":
-            cell.template = (third, third)
-        case "6":
-            cell.template = (third, twoThirds)
-        case "7":
-            cell.template = (twoThirds, 0)
-        case "8":
-            cell.template = (twoThirds, third)
-        case "9":
-            cell.template = (twoThirds, twoThirds)
-        default:
-            break
+            
+            switch puzzle.unSolvedImages[indexPath.item] {
+            
+            case "1":
+                cell.template = (0, 0)
+            case "2":
+                cell.template = (0, thirdWidth)//TODO: That's not the perfect pixel solution
+            case "3":
+                cell.template = (0, twoThirdsWidth)
+            case "4":
+                cell.template = (thirdHeight, 0)
+            case "5":
+                cell.template = (thirdHeight, thirdWidth)
+            case "6":
+                cell.template = (thirdHeight, twoThirdsWidth)
+            case "7":
+                cell.template = (twoThirdsHeight, 0)
+            case "8":
+                cell.template = (twoThirdsHeight, thirdWidth)
+            case "9":
+                cell.template = (twoThirdsHeight, twoThirdsWidth)
+            default:
+                break
+            }
         }
+        
+        
         return cell
     }
 }
@@ -129,15 +110,9 @@ extension PuzzleViewController: UICollectionViewDataSource {
 extension PuzzleViewController: UICollectionViewDelegateFlowLayout {
     //TODO: Size
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-
-        
-        
             let collectionViewHeight = collectionView.bounds.height/3//TODO: Not a perfect pixel size
             let collectionViewWidth = collectionView.bounds.width/3
             return CGSize(width: collectionViewWidth, height: collectionViewHeight)
-        
-        
-        
     }
 
     //TODO: Insets
@@ -146,19 +121,17 @@ extension PuzzleViewController: UICollectionViewDelegateFlowLayout {
             return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
     
-    
-    //+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
-    //+
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
 }
 
 extension PuzzleViewController: UICollectionViewDragDelegate {
-    //+
+    
     //As you can see, in the ‘itemsForBeginning’ method we are calling our ‘dragItem’ that will deal with the dragItem to be returned. The helper method ‘dragItem’ helps us create the itemProvider with the correct string or image content, depending on the cell we are dragging.
     func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
         //TODO: Seems like it has an isssue but I don't realy know how to imaplement it
@@ -169,7 +142,6 @@ extension PuzzleViewController: UICollectionViewDragDelegate {
             return [UIDragItem]()
         }
         
-        
         let itemProvider = NSItemProvider(object: item as NSString)
         let dragItem = UIDragItem(itemProvider: itemProvider)
         dragItem.localObject = dragItem
@@ -178,7 +150,6 @@ extension PuzzleViewController: UICollectionViewDragDelegate {
 }
 
 extension PuzzleViewController: UICollectionViewDropDelegate {
-    //+
     /*
         This method tell the DropDelegate that something is happening when the user drags a cell and drops it at a new location.
         UICollectionViewDropProposal has four types of operation which is copy,forbidden,cancel,move.In this project I have used move operation which is to arrange the puzzles.
@@ -199,10 +170,9 @@ extension PuzzleViewController: UICollectionViewDropDelegate {
             return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
         }
         
-        
         return UICollectionViewDropProposal(operation: .forbidden)
     }
-    //+
+    
     /*  Use this method to accept the dropped content and integrate it into your collection view.
         In your implementation, iterate over the items property of the coordinator object and fetch the data from each UIDragItem.
         Incorporate the data into your collection view's data source and update the collection view itself by inserting any needed items.
@@ -229,17 +199,15 @@ extension PuzzleViewController: UICollectionViewDropDelegate {
                 puzzle.unSolvedImages.swapAt(sourceIndexPath.item, destinationIndexPath.item)
                 collectionView.reloadItems(at: [sourceIndexPath, destinationIndexPath])
             }
-            //MARK: have a bit .....
             coordinator.drop(item.dragItem, toItemAt: destinationIndexPath)
         }
         
     }
-    //+
+    
     //this method is called every time a item is dropped and checks everytime if puzzle is solved or not.If unsolvedImage array equals to solved image array. Change dragInteractionEnabled to false to tell user that puzzle has solved and show Alert .Enable rightBarbutton to true to navigate to next puzzle.
     func collectionView(_ collectionView: UICollectionView, dropSessionDidEnd session: UIDropSession) {
         if puzzle.unSolvedImages == puzzle.solvedImages {
             Alert.showSolvedPuzzleAlert(on: self)
-            
             collectionView.dragInteractionEnabled = false
         }
     }
